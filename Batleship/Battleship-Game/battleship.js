5c8d856b71d2112939603b133169ebb8b580910e
cepcema <49753804+cepcema@users.noreply.github.com>
2019-06-27 19:44:02 +0300

    Add files via upload

---
+++ Batleship/Battleship-Game/battleship.js
@@ -0,0 +1,39 @@
+let model = {
+    boardsize: 7,
+    numShips: 3,
+    shipLenght: 3,
+    shipsSunk: 0,
+
+    ships: [{
+            locations: ["06", "16", "26"],
+            hits: ["", "", ""]
+        },
+        {
+            locations: ["24", "34", "44"],
+            hits: ["", "", ""]
+        },
+        {
+            locations: ["10", "11", "12"],
+            hits: ["", "", ""]
+        }
+    ],
+
+}
+
+let view = {
+    displayMessage: function (msg) {
+        let messageArea = document.getElementById(`messageArea`);
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+};
+
+
+view.displayMessage("TAP TAP MAP ")
